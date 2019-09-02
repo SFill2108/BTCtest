1af9d5eb4946c7c732d608cb3740e17a9c8be699
Fill <gcodepost@gmail.com>
2019-09-02 15:49:32 +0300

    page name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,15 @@
 import React,{Component} from 'react';
 import './Header.scss';
-import {NavLink} from "react-router-dom"
+import PageName from  './PageName/PageName.jsx';
+import {NavLink} from "react-router-dom";
+
 
 const Header = () =>{
     return(
-        <div>
-            <div>
-                Browse Available Books
-            </div>
-            <div>
-                <div>
-                    <NavLink to='/allbooks'>All Books</NavLink>
-                    <NavLink to='/mostrecent'>Most Recent</NavLink>
-                </div>
-                <div>
-                    <input type="text"/>
-                </div>
-            </div>
+        <div className='headerWrapper'>
+            <PageName />
+            
+
         </div>
     );
 }
